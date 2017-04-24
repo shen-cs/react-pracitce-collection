5be4936b44fbb7c33cb7d2ce3f9614e93fafe799
Shen Chang-Shao <stationaryfront211@gmail.com>
2017-04-25 00:11:30 +0800

    link added

--- src/components/GithubHome.js
+++ src/components/GithubHome.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Spinner from 'react-spinner-material';
 import { Link } from 'react-router';
 import '../css/github-finder.css'
 import { githubDataActionCreator } from '../redux/githubDataRedux';
 
 const mapStateToProps = (state) => ({
 	userId: state.githubData.userId,
+	spinnerVisible: state.githubUi.spinnerVisible,
 })
 
 class GithubHome extends Component {
@@ -21,13 +23,24 @@ class GithubHome extends Component {
 	}
 	render() {
 		return (
-			<div className="container">
-				<TextField
-					hintText="Please enter github user ID..."
-					onChange={this.onChangeText}/>
-				<Link to="/github-finder/result">
-					<RaisedButton label="submit" onClick={this.onSubmit} primary/>
-				</Link>
+			<div className="box">
+				<div className="container">
+					<TextField
+						hintText="Please enter github user ID..."
+						onChange={this.onChangeText}/>
+					<Link to="/github-finder/result">
+						<RaisedButton label="submit" onClick={this.onSubmit} primary/>
+					</Link>
+					
+				</div>
+				<div >
+				<Spinner
+						height={20}
+						width={20}
+						spinnerColor={"#333"}
+						spinnerWidth={2}
+						show={this.props.spinnerVisible}/>
+				</div>
 			</div>
 		)
 	}
